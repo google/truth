b93b7721cd862caaa1653577d247415038bfc65c
Christian Edward Gruber <christianedwardgruber@gmail.com>
2011-06-02 18:06:54 -0400

    rename FirstTest -> StringTest since that's all it contains.

--- src/test/java/test/org/junit/contrib/truth/StringTest.java
+++ src/test/java/test/org/junit/contrib/truth/StringTest.java
@@ -23,7 +23,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class FirstTest {
+public class StringTest {
 
 	@Test public void stringContainsFail() {
 		try {
