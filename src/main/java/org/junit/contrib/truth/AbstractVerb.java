9ee86f8fb29032188c5cee0ef23a5017ea83a064
Christian Edward Gruber <christianedwardgruber@gmail.com>
2012-03-26 08:27:18 -0400

    Remove strangely added (by eclipse) import that depends on a test class.  o.O?

--- src/main/java/org/junit/contrib/truth/AbstractVerb.java
+++ src/main/java/org/junit/contrib/truth/AbstractVerb.java
@@ -1,6 +1,5 @@
 package org.junit.contrib.truth;
 
-import org.junit.contrib.truth.delegatetest.DelegationTest;
 import org.junit.contrib.truth.subjects.Subject;
 import org.junit.contrib.truth.subjects.SubjectFactory;
 import org.junit.contrib.truth.util.GwtCompatible;
@@ -17,7 +16,7 @@ public class AbstractVerb {
   protected FailureStrategy getFailureStrategy() {
     return failureStrategy;
   }
-  
+
 	/**
 	 * Triggers the failure strategy with an empty failure message
 	 */
@@ -33,24 +32,24 @@ public class AbstractVerb {
 	}
 
   /**
-   * The recommended method of extension of Truth to new types, which is 
-   * documented in {@link DelegationTest }.  
-   * 
+   * The recommended method of extension of Truth to new types, which is
+   * documented in {@link DelegationTest }.
+   *
    * @see DelegationTest
    * @param factory a SubjectFactory<S, T> implementation
    * @returns A custom verb for the type returned by the SubjectFactory
    */
-  public <S extends Subject<S,T>, T, SF extends SubjectFactory<S, T>> 
+  public <S extends Subject<S,T>, T, SF extends SubjectFactory<S, T>>
       DelegatedVerb<S, T> about(SF factory) {
       return new DelegatedVerb<S, T>(getFailureStrategy(), factory);
   }
-  
+
   /**
    * A special Verb implementation which wraps a SubjectFactory
    */
   public static class DelegatedVerb<S extends Subject<S,T>, T>
       extends AbstractVerb {
-    
+
     private final SubjectFactory<S, T> factory;
 
     public DelegatedVerb(FailureStrategy fs, SubjectFactory<S, T> factory) {
