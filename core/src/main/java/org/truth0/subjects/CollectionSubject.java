34525a9e4488a568fe111d212c27fb5265fb9416
Christian Gruber <christianedwardgruber@gmail.com>
2012-09-05 17:29:09 -0400

    Add Google to the license of this file that was substantiall re-written on Google's dime.

--- core/src/main/java/org/truth0/subjects/CollectionSubject.java
+++ core/src/main/java/org/truth0/subjects/CollectionSubject.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2011 David Saff
  * Copyright (c) 2011 Christian Gruber
+ * Copyright (c) 2012 Google, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
