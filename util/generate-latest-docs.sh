7b2e0d69ab7f0bed397d0d49aee012a68e4edb68
Christian Edward Gruber <cgruber@google.com>
2014-10-08 18:11:26 -0700

    Fix broken apidocs site stuff.

--- util/generate-latest-docs.sh
+++ util/generate-latest-docs.sh
@@ -5,7 +5,9 @@ if [ "$TRAVIS_REPO_SLUG" == "google/truth" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing javadoc...\n"
+  
   mvn javadoc:aggregate
+  TARGET="$(pwd)/target"
 
   cd $HOME
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/google/truth gh-pages > /dev/null
@@ -14,8 +16,8 @@ if [ "$TRAVIS_REPO_SLUG" == "google/truth" ] && \
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git rm -rf api/latest 
-  mv ${HOME}/target/site/apidocs api/latest
-  git add -f api/latest
+  mv ${TARGET}/site/apidocs api/latest
+  git add -A -f api/latest
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
