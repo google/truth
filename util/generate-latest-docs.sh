351875579fb17c5bad98b5c4b9a74a17f9085a11
Christian Gruber <christianedwardgruber@gmail.com>
2014-10-08 18:00:39 -0700

    Stop using the travis branch, and grab a javadoc:aggregate output instead of going through a jar.

--- util/generate-latest-docs.sh
+++ util/generate-latest-docs.sh
@@ -5,20 +5,19 @@ if [ "$TRAVIS_REPO_SLUG" == "google/truth" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing javadoc...\n"
-  mvn javadoc:jar
+  mvn javadoc:aggregate
 
   cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/google/truth gh-pages > /dev/null
   
   cd gh-pages
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
   git rm -rf api/latest 
-  mkdir -p api/latest
-  unzip ${HOME}/core/target/*-SNAPSHOT-javadoc.jar -d api/latest
+  mv ${HOME}/target/site/apidocs api/latest
   git add -f api/latest
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Published Javadoc & JDiff to gh-pages.\n"
+  echo -e "Published Javadoc to gh-pages.\n"
 fi
--- util/publish-snapshot-on-commit.sh
+++ util/publish-snapshot-on-commit.sh
@@ -7,9 +7,8 @@ if [ "$TRAVIS_REPO_SLUG" == "google/truth" -a \
   echo -e "Publishing maven snapshot...\n"
 
   cd ${HOME}
-  git clone --quiet --branch=travis https://github.com/google/truth travis > /dev/null
   
-  mvn clean deploy --settings="$HOME/travis/settings.xml" -DskipTests=true -Dmaven.javadoc.skip=true
+  mvn clean deploy --settings="${HOME}/util/settings.xml" -DskipTests=true -Dmaven.javadoc.skip=true
 
   echo -e "Published maven snapshot"
 fi
