1e6323199605eb463a8547c1019775eea1a58bfa
kak <kak@google.com>
2014-10-24 12:59:45 -0700

    Update deprecation instructions for exactly() methods. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=78457656

--- core/src/main/java/com/google/common/truth/CollectionSubject.java
+++ core/src/main/java/com/google/common/truth/CollectionSubject.java
@@ -137,10 +137,10 @@ public class CollectionSubject<S extends CollectionSubject<S, T, C>, T, C extend
      * only these objects or fails. This copes with duplicates in both the
      * Collection and the parameters. It makes no attestation about order
      * unless {@code inOrder()} is explicitly called.
+     *
+     * @deprecated Use {@link IterableSubject#containsExactly(Object)} instead.
      */
-    // TODO(user): Deprecate this once the replacement method is added!
-    // @deprecated Use {@link IterableSubject#containsExactly(Object)} instead.
-    // @Deprecated
+    @Deprecated
     public Ordered exactly(T first) {
       // We can't inline containsExactly(first) because of the (bogus) return type!
       return exactlyAs(accumulate(first));
@@ -151,10 +151,10 @@ public class CollectionSubject<S extends CollectionSubject<S, T, C>, T, C extend
      * only these objects or fails. This copes with duplicates in both the
      * Collection and the parameters. It makes no attestation about order
      * unless {@code inOrder()} is explicitly called.
+     *
+     * @deprecated Use {@link IterableSubject#containsExactly(Object...)} instead.
      */
-    // TODO(user): Deprecate this once the replacement method is added!
-    // @deprecated Use {@link IterableSubject#containsExactly(Object, Object, Object...)} instead.
-    // @Deprecated
+    @Deprecated
     public Ordered exactly(T first, T second, T ... rest) {
       return CollectionSubject.this.containsOnlyElements(first, second, rest);
     }
@@ -164,10 +164,10 @@ public class CollectionSubject<S extends CollectionSubject<S, T, C>, T, C extend
      * provided collection and only these objects or fails. This copes with
      * duplicates in both the Collection and the parameters. It makes no
      * attestation about order unless {@code inOrder()} is explicitly called.
+     *
+     * @deprecated Use {@link IterableSubject#containsExactlyElementsIn(Iterable<?>)} instead.
      */
-    // TODO(user): Deprecate this once the replacement method is added!
-    // @deprecated Use {@link IterableSubject#containsExactlyIn(Iterable<?>)} instead.
-    // @Deprecated
+    @Deprecated
     public Ordered exactlyAs(Iterable<T> required) {
       return CollectionSubject.this.containsOnlyElementsIn(required);
     }
