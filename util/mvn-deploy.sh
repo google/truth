329ae37bae79e415ea66a48a8554515666bd9aa5
Christian Edward Gruber <cgruber@google.com>
2015-06-04 11:54:34 -0700

    Update release script to use the upstream release profile.

--- util/mvn-deploy.sh
+++ util/mvn-deploy.sh
@@ -17,4 +17,5 @@ if [ "${keystatus}" != "pub" ]; then
   exit 1
 fi
 
-mvn ${params} clean site:jar -Dgpg.skip=false -Dgpg.keyname=${key} deploy
+mvn ${params} clean site:jar -P sonatype-oss-release -Dgpg.keyname=${key} deploy
+
