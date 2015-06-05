6301273ca0486c7e3716cca28ccfd592b8507dac
Christian Gruber <cgruber@google.com>
2015-06-05 16:26:27 -0700

    Update mvn-deploy.sh to be more convenient for people with only one key, and with better error messages.

--- util/mvn-deploy.sh
+++ util/mvn-deploy.sh
@@ -1,21 +1,57 @@
 #!/bin/bash
-if [ $# -lt 1 ]; then
-  echo "usage $0 <ssl-key> [<param> ...]"
-  exit 1;
-fi
-key=${1}
-shift
-params=${@}
+keys="$(gpg --list-keys | grep ^pub | sed 's#/# #' | awk '{ print $3 }')"
+key_count="$(echo ${keys} | wc -w)"
 
-#validate key
-keystatus=$(gpg --list-keys | grep ${key} | awk '{print $1}')
-if [ "${keystatus}" != "pub" ]; then
-  echo "Could not find public key with label ${key}"
-  echo -n "Available keys from: "
-  gpg --list-keys | grep --invert-match '^sub'
+seen=""
+while [[ $# > 0 ]] ; do
+  param="$1"
+  case $param in
+    --signing-key)
+      # disambiguating or overriding key
+      key="$2"
+      shift
+    ;;
+    *)
+      seen="${seen} ${param}"
+    ;;
+  esac
+  shift
+done
+params=${seen}
 
+if [[ ${key_count} -lt 1 ]]; then
+  echo ""
+  echo "You are attempting to deploy a maven release without a GPG signing key."
+  echo "You need to generate a signing key in accordance with the instructions"
+  echo "found at http://blog.sonatype.com/2010/01/how-to-generate-pgp-signatures-with-maven"
   exit 1
 fi
 
-mvn ${params} clean site:jar -P sonatype-oss-release -Dgpg.keyname=${key} deploy
+# if a key is specified, use that, else use the default, unless there are many
+if [[ -n "${key}" ]]; then 
+  #validate key
+  keystatus=$(gpg --list-keys | grep ${key} | awk '{print $1}')
+  if [ "${keystatus}" != "pub" ]; then
+    echo ""
+    echo "Could not find public key with label \"${key}\""
+    echo ""
+    echo "Available keys from: "
+    gpg --list-keys | grep --invert-match '^sub'
+    exit 1
+  fi
+
+  key_param="-Dgpg.keyname=${key}"
+elif [ ${key_count} -gt 1 ]; then
+  echo ""
+  echo "You are attempting to deploy a maven release but have more than one GPG"
+  echo "signing key and did not specify which one you wish to sign with."
+  echo ""
+  echo "usage $0 [--signing-key <ssl-key>] [<maven params> ...]"
+  echo ""
+  echo -n "Available keys from: "
+  gpg --list-keys | grep --invert-match '^sub'
+  exit 1;
+fi
+
+mvn ${params} clean site:jar -P sonatype-oss-release ${key_param} deploy
 
