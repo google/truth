80ac92a94f2d1f1f75ba89f6b98d883073d68562
kak <kak@google.com>
2016-06-22 10:44:35 -0700

    Rename EmployeeTest to EmployeeSubjectTest. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=125582067

--- core/src/test/java/com/google/common/truth/extension/EmployeeSubjectTest.java
+++ core/src/test/java/com/google/common/truth/extension/EmployeeSubjectTest.java
@@ -25,7 +25,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public final class EmployeeTest {
+public final class EmployeeSubjectTest {
 
   // Note: not real employee IDs :-)
 
