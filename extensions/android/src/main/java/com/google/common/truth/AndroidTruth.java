5c20033cb2bb684a64aa6af6ac0bc7a3cd100e1d
KevinLeighCrain <kevinleighcrain@gmail.com>
2016-11-08 08:49:29 +0200

    Add subject(`ViewSubject`) to assert android view objects
    - Running tests currently fail on my side due to duplicate `Platform`
    classes, so test class will follow after

--- extensions/android/src/main/java/com/google/common/truth/AndroidTruth.java
+++ extensions/android/src/main/java/com/google/common/truth/AndroidTruth.java
@@ -1,7 +1,18 @@
 package com.google.common.truth;
 
+import android.view.View;
+
+import javax.annotation.Nullable;
+
+import static com.google.common.truth.Truth.assertAbout;
+
 /**
  * @author Kevin Leigh Crain
  */
 public class AndroidTruth {
+
+    public static ViewSubject assertThat(@Nullable View target) {
+        return assertAbout(ViewSubject.views()).that(target);
+    }
+
 }
--- extensions/android/src/main/java/com/google/common/truth/ViewSubject.java
+++ extensions/android/src/main/java/com/google/common/truth/ViewSubject.java
@@ -8,7 +8,27 @@ import javax.annotation.Nullable;
  * @author Kevin Leigh Crain
  */
 public class ViewSubject extends Subject<ViewSubject, View> {
+
     public ViewSubject(FailureStrategy failureStrategy, @Nullable View actual) {
         super(failureStrategy, actual);
     }
+
+    public final void isVisible() {
+        if(actual().getVisibility() != View.VISIBLE) fail("is not visible");
+    }
+
+    public final void isNotVisible() {
+        if(actual().getVisibility() == View.VISIBLE) fail("is visible");
+    }
+
+    public static SubjectFactory<ViewSubject, View> views() {
+        return FACTORY;
+    }
+
+    private static final SubjectFactory<ViewSubject, View> FACTORY = new SubjectFactory<ViewSubject, View>() {
+        @Override
+        public ViewSubject getSubject(FailureStrategy fs, View that) {
+            return new ViewSubject(fs, that);
+        }
+    };
 }
