84096db48dfe52cf21f669d65e8aa74f88245743
KevinLeighCrain <kevinleighcrain@gmail.com>
2016-11-08 07:47:39 +0200

    Add base Android truth extension module

---
+++ extensions/android/src/main/java/com/google/common/truth/AndroidTruth.java
@@ -0,0 +1,7 @@
+package com.google.common.truth;
+
+/**
+ * @author Kevin Leigh Crain
+ */
+public class AndroidTruth {
+}
---
+++ extensions/android/src/test/java/com/google/common/truth/AndroidTruthTest.java
@@ -0,0 +1,8 @@
+package com.google.common.truth;
+
+/**
+ * @author Kevin Leigh Crain
+ */
+public class AndroidTruthTest {
+
+}
