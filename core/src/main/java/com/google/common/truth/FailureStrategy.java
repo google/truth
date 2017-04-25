05e7ffb3d38f26afa7a68c9bd8ddbd9be779008f
diamondm <diamondm@google.com>
2017-04-24 18:05:29 -0700

    Introduce AbstractFailureStrategy and add javadocs to FailureStrategy that references both AbstractFailureStrategy and the common strategy implementations.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=154124248

--- core/src/main/java/com/google/common/truth/FailureStrategy.java
+++ core/src/main/java/com/google/common/truth/FailureStrategy.java
@@ -17,11 +17,34 @@ package com.google.common.truth;
 
 import java.util.Arrays;
 
+/**
+ * A {@code FailureStrategy} defines how assertion failures are handled. In {@link Truth}, failures
+ * {@linkplain Truth#THROW_ASSERTION_ERROR throw AssertionErrors}; other assertion entry-points like
+ * {@link Expect} and {@link TruthJUnit#assume} have different failure behavior.
+ *
+ * <p>It should generally be unnecessary for Truth SPI developers to define their own {@code
+ * FailureStrategy} implementations, they should instead use {@link Subject#check} to invoke other
+ * subjects using the current strategy. When you really do need to creatae your own strategy prefer
+ * to extend {@link AbstractFailureStrategy} rather than this class directly.
+ *
+ * <p>Implementation Note: the concrete methods in this class will be made abstract in the near
+ * future (see b/37472530); please do not depend on these existing implementations in new code,
+ * prefer {@code AbstractFailureStrategy}.
+ */
 public abstract class FailureStrategy {
+  /**
+   * Report an assertion failure with a text message. This method should generally delegate to
+   * {@link #fail(String, Throwable)}.
+   */
   public void fail(String message) {
     fail(message, null);
   }
 
+  /**
+   * Report an assertion failure with a text message and a throwable that indicates the cause of the
+   * failure. This will be reported as the cause of the exception raised by this method (if one is
+   * thrown) or otherwise recorded by the strategy as the underlying cause of the failure.
+   */
   public void fail(String message, Throwable cause) {
     AssertionError up = new AssertionError(message);
     if (cause == null) {
@@ -38,10 +61,21 @@ public abstract class FailureStrategy {
     throw up;
   }
 
+  /**
+   * Convenience method to report string-comparison failures with more detail (e.g. character
+   * differences). This method should generally delegate to {@link #failComparing(String,
+   * CharSequence, CharSequence, Throwable)}.
+   */
   public void failComparing(String message, CharSequence expected, CharSequence actual) {
     failComparing(message, expected, actual, null);
   }
 
+  /**
+   * Convenience method to report string-comparison failures with more detail (e.g. character
+   * differences), along with a throwable that indicates the cause of the failure. This will be
+   * reported as the cause of the exception raised by this method (if one is thrown) or otherwise
+   * recorded by the strategy as the underlying cause of the failure.
+   */
   public void failComparing(
       String message, CharSequence expected, CharSequence actual, Throwable cause) {
     fail(StringUtil.messageFor(message, expected, actual), cause);
