0f3b90402f28d7b06c208e2b1424a289909fb3f3
ronshapiro <ronshapiro@google.com>
2017-05-25 11:08:53 -0700

    Only test on Java 8
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=157128188

--- util/publish-snapshot-on-commit.sh
+++ util/publish-snapshot-on-commit.sh
@@ -1,7 +1,7 @@
 # see https://coderwall.com/p/9b_lfq
 
 if [ "$TRAVIS_REPO_SLUG" == "google/truth" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "$JDK_FOR_PUBLISHING" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Publishing maven snapshot...\n"
