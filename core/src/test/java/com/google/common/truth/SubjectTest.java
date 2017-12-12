ae4ec76a0034a1c5b0486abfddca5252d2974db9
cpovirk <cpovirk@google.com>
2017-12-12 11:32:11 -0800

    Remove another low-value test.
    THROW_ASSERTION_ERROR is no longer public API, and we already check that causes are present when using public APIs like ThrowableSubject.
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=178789682

--- core/src/test/java/com/google/common/truth/SubjectTest.java
+++ core/src/test/java/com/google/common/truth/SubjectTest.java
@@ -601,18 +601,6 @@ public class SubjectTest {
   }
 
   @Test
-  public void throwableHasInitedCause() {
-    NullPointerException cause = new NullPointerException();
-    String msg = "foo";
-    try {
-      Truth.THROW_ASSERTION_ERROR.fail(msg, cause);
-    } catch (AssertionError expected) {
-      assertThat(expected).hasMessageThat().isEqualTo(msg);
-      assertThat(expected).hasCauseThat().isSameAs(cause);
-    }
-  }
-
-  @Test
   @SuppressWarnings("EqualsIncompatibleType")
   public void equalsThrowsUSOE() {
     try {
