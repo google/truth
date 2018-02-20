google/truth

commit 06e03c143e3ed0cba608fbd3c610d9d7e5701b64

Run truth subject tests under j2cl.
Changes includes:
- Having the ExpectFailure to implement a wrapped interface of TestRule to allow GWT swapping
- Add BaseSubjectTestCase class for subjec tests to extend which setup a ExpectFailure in `@Before` and ensure failure caught in `@After`.
- Add a small build rule similar to GenTestRules but for j2cl_tests.
- Migrate two subject tests as a start to prove the concept

RELNOTES=n/a

-------------
Created by MOE: https://github.com/google/moe
MOE_MIGRATED_REVID=177901707

core/src/main/java/com/google/common/truth/ExpectFailure.java
@@ -17,11 +17,11 @@ package com.google.common.truth;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.truth.StringUtil.format;
 
-import com.google.common.base.Throwables;
+import com.google.common.annotations.GwtIncompatible;
 import com.google.common.truth.Truth.AssertionErrorWithCause;
 import javax.annotation.Nullable;
-import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
@@ -60,7 +60,7 @@ import org.junit.runners.model.Statement;
  * because it also checks that the assertion you're testing correctly calls {@link
  * FailureStrategy#fail} only once.
  */
-public final class ExpectFailure implements TestRule {
+public final class ExpectFailure implements Platform.JUnitTestRule {
   private final FailureStrategy strategy =
       new AbstractFailureStrategy() {
         @Override
@@ -111,6 +111,31 @@ public final class ExpectFailure implements TestRule {
     return StandardSubjectBuilder.forCustomFailureStrategy(strategy);
   }
 
+  /**
+   * Enters rule context to be ready to capture failures.
+   *
+   * <p>This should be rarely used directly, except if this class is as a long living object but not
+   * as a JUnit rule, like truth subject tests where for GWT compatible reasons.
+   */
+  void enterRuleContext() {
+    this.inRuleContext = true;
+  }
+
+  /** Leaves rule context and verify if a failure has been caught if it's expected. */
+  void leaveRuleContext() {
+    this.inRuleContext = false;
+  }
+
+  /**
+   * Ensures a failure is caught if it's expected (i.e., {@link #whenTesting} is called) and throws
+   * error if not.
+   */
+  void ensureFailureCaught() {
+    if (failureExpected && failure == null) {
+      throw new AssertionError("ExpectFailure.whenTesting() invoked, but no failure was caught.");
+    }
+  }
+
   /** Returns the captured failure, if one occurred. */
   public AssertionError getFailure() {
     if (failure == null) {
@@ -127,10 +152,9 @@ public final class ExpectFailure implements TestRule {
     if (failure != null) {
       // TODO(diamondm) is it worthwhile to add the failures as suppressed exceptions?
       throw new AssertionError(
-          String.format(
+          format(
               "ExpectFailure.whenTesting() caught multiple failures:\n\n%s\n\n%s\n",
-              Throwables.getStackTraceAsString(failure),
-              Throwables.getStackTraceAsString(captured)));
+              Platform.getStackTraceAsString(failure), Platform.getStackTraceAsString(captured)));
     }
     failure = captured;
   }
@@ -144,7 +168,7 @@ public final class ExpectFailure implements TestRule {
    */
   public static AssertionError expectFailure(StandardSubjectBuilderCallback assertionCallback) {
     ExpectFailure expectFailure = new ExpectFailure();
-    expectFailure.inRuleContext = true; // safe since this instance doesn't leave this method
+    expectFailure.enterRuleContext(); // safe since this instance doesn't leave this method
     assertionCallback.invokeAssertion(expectFailure.whenTesting());
     return expectFailure.getFailure();
   }
@@ -194,22 +218,20 @@ public final class ExpectFailure implements TestRule {
   }
 
   @Override
+  @GwtIncompatible("org.junit.rules.TestRule")
   public Statement apply(final Statement base, Description description) {
     checkNotNull(base);
     checkNotNull(description);
     return new Statement() {
       @Override
       public void evaluate() throws Throwable {
-        inRuleContext = true;
+        enterRuleContext();
         try {
           base.evaluate();
         } finally {
-          inRuleContext = false;
+          leaveRuleContext();
         }
-        if (failureExpected && failure == null) {
-          throw new AssertionError(
-              "ExpectFailure.whenTesting() invoked, but no failure was caught.");
-        }
+        ensureFailureCaught();
       }
     };
   }

core/src/main/java/com/google/common/truth/Platform.java
@@ -15,6 +15,7 @@
  */
 package com.google.common.truth;
 
+import com.google.common.base.Throwables;
 import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -23,6 +24,7 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.junit.ComparisonFailure;
+import org.junit.rules.TestRule;
 
 /**
  * Extracted routines that need to be swapped in for GWT, to allow for minimal deltas between the
@@ -193,4 +195,17 @@ final class Platform {
       return message == null ? clazz : clazz + ": " + message;
     }
   }
+
+  /** Returns a human readable string representation of the throwable's stack trace. */
+  static String getStackTraceAsString(Throwable throwable) {
+    return Throwables.getStackTraceAsString(throwable);
+  }
+
+  /**
+   * Wrapping interface of {@link TestRule} to be used within truth.
+   *
+   * <p>Note that the sole purpose of this interface is to allow it to be swapped in GWT
+   * implementation.
+   */
+  interface JUnitTestRule extends TestRule {}
 }

core/src/main/java/com/google/common/truth/super/com/google/common/truth/Platform.java
@@ -143,6 +143,18 @@ final class Platform {
     return false;
   }
 
+  /** Returns a human readable string representation of the throwable's stack trace. */
+  static String getStackTraceAsString(Throwable throwable) {
+    // TODO(cpovirk): Write a naive implementation that at least dumps the main exception's stack.
+    return throwable.toString();
+  }
+
+  /**
+   * A GWT-swapped version of test rule interface that does nothing. All methods extended from
+   * {@link org.junit.rules.TestRule} needs to be stripped.
+   */
+  interface JUnitTestRule {}
+
   // TODO(user): Move this logic to a common location.
   private static NativeRegExp compile(String pattern) {
     return new NativeRegExp(pattern);

core/src/test/java/com/google/common/truth/BaseSubjectTestCase.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2017 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.common.truth;
+
+import org.junit.After;
+import org.junit.Before;
+
+/** Base class for truth subject tests to extend. */
+abstract class BaseSubjectTestCase {
+
+  final ExpectFailure expectFailure = new ExpectFailure();
+
+  @Before
+  public void setupExpectFailure() {
+    expectFailure.enterRuleContext(); // safe since @After forces leaving the context
+  }
+
+  @After
+  public void ensureExpectedFailureCaught() {
+    expectFailure.leaveRuleContext();
+  }
+}

core/src/test/java/com/google/common/truth/AtomicLongMapSubjectTest.java
@@ -19,7 +19,6 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.fail;
 
 import com.google.common.util.concurrent.AtomicLongMap;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -30,9 +29,7 @@ import org.junit.runners.JUnit4;
  * @author Kurt Alfred Kluever
  */
 @RunWith(JUnit4.class)
-public final class AtomicLongMapSubjectTest {
-
-  @Rule public final ExpectFailure expectFailure = new ExpectFailure();
+public final class AtomicLongMapSubjectTest extends BaseSubjectTestCase {
 
   @Test
   public void isEqualToFail() {

core/src/test/java/com/google/common/truth/BigDecimalSubjectTest.java
@@ -19,7 +19,6 @@ import static com.google.common.truth.Truth.assertThat;
 import static java.math.BigDecimal.TEN;
 
 import java.math.BigDecimal;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -30,9 +29,7 @@ import org.junit.runners.JUnit4;
  * @author Kurt Alfred Kluever
  */
 @RunWith(JUnit4.class)
-public class BigDecimalSubjectTest {
-  @Rule public final ExpectFailure expectFailure = new ExpectFailure();
-
+public class BigDecimalSubjectTest extends BaseSubjectTestCase {
   @SuppressWarnings("TruthSelfEquals")
   @Test
   public void isEqualTo() {

core/src/test/java/com/google/common/truth/ExpectFailureNonRuleTest.java
@@ -1,0 +1,137 @@
+/*
+ * Copyright (c) 2011 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.common.truth;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link ExpectFailure} not used as JUnit's TestRule. */
+@RunWith(JUnit4.class)
+public class ExpectFailureNonRuleTest {
+
+  @Test
+  public void testExpect_userThrowExceptionInSubject_shouldPropagate() throws Exception {
+    final List<Failure> reportedFailure = Lists.newArrayList();
+    RunNotifier runNotifier = new RunNotifier();
+    runNotifier.addListener(
+        new RunListener() {
+          @Override
+          public void testFailure(Failure failure) throws Exception {
+            reportedFailure.add(failure);
+          }
+        });
+
+    Runner runner = new JUnit4(ExpectFailureThrowInSubject.class);
+    runner.run(runNotifier);
+
+    assertThat(reportedFailure).hasSize(2);
+    assertThat(reportedFailure.get(0).getException())
+        .hasMessageThat()
+        .contains("Throw deliberately");
+    assertThat(reportedFailure.get(1).getException())
+        .hasMessageThat()
+        .contains("ExpectFailure.whenTesting() invoked, but no failure was caught.");
+  }
+
+  @Test
+  public void testExpect_userThrowExceptionAfterSubject_shouldPropagate() throws Exception {
+    final List<Failure> reportedFailure = Lists.newArrayList();
+    RunNotifier runNotifier = new RunNotifier();
+    runNotifier.addListener(
+        new RunListener() {
+          @Override
+          public void testFailure(Failure failure) throws Exception {
+            reportedFailure.add(failure);
+          }
+        });
+
+    Runner runner = new JUnit4(ExpectFailureThrowAfterSubject.class);
+    runner.run(runNotifier);
+
+    assertThat(reportedFailure).hasSize(2);
+    assertThat(reportedFailure.get(0).getException())
+        .hasMessageThat()
+        .contains("Throw deliberately");
+    assertThat(reportedFailure.get(1).getException())
+        .hasMessageThat()
+        .contains("ExpectFailure.whenTesting() invoked, but no failure was caught.");
+  }
+
+  /**
+   * A test supporting test class which will fail because method in a subject will throw exception.
+   */
+  public static class ExpectFailureThrowInSubject {
+
+    final ExpectFailure expectFailure = new ExpectFailure();
+
+    @Before
+    public void setupExpectFailure() {
+      expectFailure.enterRuleContext();
+    }
+
+    @After
+    public void ensureFailureCaught() {
+      expectFailure.ensureFailureCaught();
+      expectFailure.leaveRuleContext();
+    }
+
+    @Test
+    public void testExpect_throwInSubject_shouldPropagate() {
+      expectFailure.whenTesting().that(throwingMethod()).isEqualTo(4);
+    }
+  }
+
+  /**
+   * A test supporting test class which will fail because method after a subject will throw
+   * exception.
+   */
+  public static class ExpectFailureThrowAfterSubject {
+
+    final ExpectFailure expectFailure = new ExpectFailure();
+
+    @Before
+    public void setupExpectFailure() {
+      expectFailure.enterRuleContext();
+    }
+
+    @After
+    public void ensureFailureCaught() {
+      expectFailure.ensureFailureCaught();
+      expectFailure.leaveRuleContext();
+    }
+
+    @Test
+    public void testExpect_throwInSubject_shouldPropagate() {
+      expectFailure.whenTesting().that(4).isEqualTo(4); // No failure being caught
+      long unused = throwingMethod();
+    }
+  }
+
+  private static long throwingMethod() {
+    throw new RuntimeException("Throw deliberately");
+  }
+}

core/src/test/java/com/google/common/truth/ExpectFailureRuleTest.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (c) 2011 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.common.truth;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.annotations.GwtIncompatible;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link ExpectFailure} used as JUnit {@link Rule).*/
+@RunWith(JUnit4.class)
+@GwtIncompatible("org.junit.Rule")
+public class ExpectFailureRuleTest {
+  @Rule public final ExpectFailure expectFailure = new ExpectFailure();
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void expectFail_captureFailureAsExpected() {
+    expectFailure.whenTesting().that(4).isNotEqualTo(4);
+    assertThat(expectFailure.getFailure()).hasMessageThat().contains("<4> is not equal to <4>");
+  }
+
+  @Test
+  public void expectFail_passesIfUnused() {
+    assertThat(4).isEqualTo(4);
+  }
+
+  @Test
+  public void expectFail_failsAfterTest() {
+    expectFailure.whenTesting().that(4).isEqualTo(4);
+    thrown.expectMessage("ExpectFailure.whenTesting() invoked, but no failure was caught.");
+  }
+
+  @Test
+  public void expectFail_throwInSubject_shouldPropagateOriginalException() {
+    thrown.expectMessage("Throwing deliberately");
+    expectFailure.whenTesting().that(throwingMethod()).isEqualTo(2);
+  }
+
+  @Test
+  public void expectFail_throwAfterSubject_shouldPropagateOriginalException() {
+    expectFailure.whenTesting().that(2).isEqualTo(2);
+    thrown.expectMessage("Throwing deliberately");
+    throwingMethod();
+  }
+
+  private static long throwingMethod() {
+    throw new RuntimeException("Throwing deliberately");
+  }
+}

commit f607e01ab24d9a6019d40659a36ab24d54e273ae

Fix use of ExpectFailure from Truth's own tests, which we accidentally broke in 06e03c143e3ed0cba608fbd3c610d9d7e5701b64.
Also, use ExpectFailure in different ways from GWT and non-GWT tests. For GWT tests, we're doing the best we can, but for non-GWT tests, we can go back to using @Rule.

RELNOTES=n/a

-------------
Created by MOE: https://github.com/google/moe
MOE_MIGRATED_REVID=186306680

core/src/main/java/com/google/common/truth/ExpectFailure.java
@@ -120,7 +120,9 @@ public final class ExpectFailure implements Platform.JUnitTestRule {
    */
   void ensureFailureCaught() {
     if (failureExpected && failure == null) {
-      throw new AssertionError("ExpectFailure.whenTesting() invoked, but no failure was caught.");
+      throw new AssertionError(
+          "ExpectFailure.whenTesting() invoked, but no failure was caught."
+              + Platform.EXPECT_FAILURE_WARNING_IF_GWT);
     }
   }
 

core/src/main/java/com/google/common/truth/Platform.java
@@ -140,4 +140,6 @@ final class Platform {
    * implementation.
    */
   interface JUnitTestRule extends TestRule {}
+
+  static final String EXPECT_FAILURE_WARNING_IF_GWT = "";
 }

core/src/main/java/com/google/common/truth/super/com/google/common/truth/Platform.java
@@ -141,6 +141,16 @@ final class Platform {
    */
   interface JUnitTestRule {}
 
+  static final String EXPECT_FAILURE_WARNING_IF_GWT =
+      " Note: One possible reason for a failure not to be caught is for the test to throw some "
+          + "other exception before the failure would have happened. Under GWT, such an exception "
+          + "is hidden by this message. The non-GWT tests do not have this problem, so you may "
+          + "wish to debug them first. If you're still having this problem, consider temporarily "
+          + "modifying the GWT copy of BaseSubjectTestCase to remove the call to "
+          + "ensureFailureCaught(). Removing that call will let any other exception fall through. "
+          + "(But of course it will also prevent the test from verifying that the expected failure "
+          + "occurred.)";
+
   // TODO(user): Move this logic to a common location.
   private static NativeRegExp compile(String pattern) {
     return new NativeRegExp(pattern);

core/src/test/java/com/google/common/truth/super/com/google/common/truth/BaseSubjectTestCase.java
@@ -15,9 +15,21 @@
  */
 package com.google.common.truth;
 
-import org.junit.Rule;
+import org.junit.After;
+import org.junit.Before;
 
-/** Base class for truth subject tests to extend. */
 public abstract class BaseSubjectTestCase {
-  @Rule public final ExpectFailure expectFailure = new ExpectFailure();
+
+  final ExpectFailure expectFailure = new ExpectFailure();
+
+  @Before
+  public void setupExpectFailure() {
+    expectFailure.enterRuleContext(); // safe since @After forces leaving the context
+  }
+
+  @After
+  public void ensureExpectedFailureCaught() {
+    expectFailure.leaveRuleContext();
+    expectFailure.ensureFailureCaught();
+  }
 }