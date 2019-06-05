5373725691880fc04e303f6961fd1694b5d6d1bb
cpovirk <cpovirk@google.com>
2019-06-05 14:51:48 -0700

    Provide a tool to migrate from actual() and getSubject() to a local `actual` field.
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=251724266

---
+++ refactorings/src/test/java/com/google/common/truth/refactorings/StoreActualValueInFieldTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2019 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.truth.refactorings;
+
+import com.google.errorprone.BugCheckerRefactoringTestHelper;
+import com.google.errorprone.CompilationTestHelper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** @author cpovirk@google.com (Chris Povirk) */
+@RunWith(JUnit4.class)
+public class StoreActualValueInFieldTest {
+  private BugCheckerRefactoringTestHelper refactoringHelper;
+  private CompilationTestHelper compilationHelper;
+
+  @Before
+  public void setUp() {
+    compilationHelper =
+        CompilationTestHelper.newInstance(StoreActualValueInField.class, getClass());
+    refactoringHelper =
+        BugCheckerRefactoringTestHelper.newInstance(new StoreActualValueInField(), getClass());
+  }
+
+  @Test
+  public void testPositiveCase() {
+    compilationHelper.addSourceFile("StoreActualValueInFieldPositiveCases.java").doTest();
+  }
+
+  @Test
+  public void testNegativeCase() {
+    compilationHelper.addSourceFile("StoreActualValueInFieldNegativeCases.java").doTest();
+  }
+
+  @Test
+  public void refactoring() {
+    refactoringHelper
+        .addInput("StoreActualValueInFieldPositiveCases.java")
+        .addOutput("StoreActualValueInFieldPositiveCases_expected.java")
+        .doTest();
+  }
+}
