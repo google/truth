003ae1050c218a1c57d18359d14ea2f1f4169d2d
Daniel Lin <ephemient@gmail.com>
2020-07-11 15:27:51 -0400

    Add tests to JUnit 5 extension

--- extensions/junit5/src/main/java/com/google/common/truth/junit5/Expect.java
+++ extensions/junit5/src/main/java/com/google/common/truth/junit5/Expect.java
@@ -30,53 +30,53 @@ import java.util.List;
  * </pre>
  */
 public class Expect implements Extension, ParameterResolver, AfterEachCallback {
-    @Override
-    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
-        return parameterContext.getParameter().getType() == StandardSubjectBuilder.class;
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+    return parameterContext.getParameter().getType() == StandardSubjectBuilder.class;
+  }
+
+  @Override
+  public StandardSubjectBuilder resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+    ExtensionContext.Store store = extensionContext.getStore(ExtensionContext.Namespace.create(Expect.class));
+    return store.getOrComputeIfAbsent(extensionContext.getUniqueId(), key -> new Holder(), Holder.class).get();
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) {
+    ExtensionContext.Store store = context.getStore(ExtensionContext.Namespace.create(Expect.class));
+    Holder holder = store.get(context.getUniqueId(), Holder.class);
+    List<AssertionError> failures = null;
+    if (holder != null) {
+      synchronized (holder) {
+        failures = holder.failures;
+        if (failures == null) {
+          throw new IllegalStateException();
+        }
+        holder.failures = null;
+      }
+    }
+    if (failures != null && !failures.isEmpty()) {
+      throw failures.size() == 1 ? failures.get(0) : new MultipleFailuresError(null, failures);
+    }
+  }
+
+  private static class Holder implements FailureStrategy, ExtensionContext.Store.CloseableResource {
+    private List<AssertionError> failures = new ArrayList<>();
+
+    private StandardSubjectBuilder get() {
+      return StandardSubjectBuilder.forCustomFailureStrategy(this);
     }
 
     @Override
-    public StandardSubjectBuilder resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
-        ExtensionContext.Store store = extensionContext.getStore(ExtensionContext.Namespace.create(Expect.class));
-        return store.getOrComputeIfAbsent(extensionContext.getUniqueId(), key -> new Holder(), Holder.class).get();
+    public synchronized void fail(AssertionError failure) {
+      failures.add(failure);
     }
 
     @Override
-    public void afterEach(ExtensionContext context) {
-        ExtensionContext.Store store = context.getStore(ExtensionContext.Namespace.create(Expect.class));
-        Holder holder = store.get(context.getUniqueId(), Holder.class);
-        List<AssertionError> failures = null;
-        if (holder != null) {
-            synchronized (holder) {
-                failures = holder.failures;
-                if (failures == null) {
-                    throw new IllegalStateException();
-                }
-                holder.failures = null;
-            }
-        }
-        if (failures != null && !failures.isEmpty()) {
-            throw failures.size() == 1 ? failures.get(0) : new MultipleFailuresError(null, failures);
-        }
+    public synchronized void close() {
+      if (failures != null) {
+        throw new IllegalStateException();
+      }
     }
-
-    private static class Holder implements FailureStrategy, ExtensionContext.Store.CloseableResource {
-        private List<AssertionError> failures = new ArrayList<>();
-
-        private StandardSubjectBuilder get() {
-            return StandardSubjectBuilder.forCustomFailureStrategy(this);
-        }
-
-        @Override
-        public synchronized void fail(AssertionError failure) {
-            failures.add(failure);
-        }
-
-        @Override
-        public synchronized void close() {
-            if (failures != null) {
-                throw new IllegalStateException();
-            }
-        }
-    }
+  }
 }
