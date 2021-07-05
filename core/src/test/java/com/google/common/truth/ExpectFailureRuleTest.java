c95ab0ea252927e97b47f571ebaf7999b2b8b065
Google Java Core Libraries <java-team-github-bot@google.com>
2021-07-05 13:15:45 -0700

    Fix Javadoc links in c.g.common.
    RELNOTES=n/a
    PiperOrigin-RevId: 383167534

--- core/src/test/java/com/google/common/truth/ExpectFailureRuleTest.java
+++ core/src/test/java/com/google/common/truth/ExpectFailureRuleTest.java
@@ -24,7 +24,7 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Tests for {@link ExpectFailure} used as JUnit {@link Rule).*/
+/** Tests for {@link ExpectFailure} used as JUnit {@link Rule}. */
 @RunWith(JUnit4.class)
 @GwtIncompatible("org.junit.Rule")
 public class ExpectFailureRuleTest {
