bd6b587b229a3465c753fd005e81036bf8cc2be0
Chris Povirk <cpovirk@google.com>
2024-02-02 13:43:31 -0500

    Release 1.4.0 javadoc pushed to gh-pages.

---
+++ api/1.4.0/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.google.common.truth"},{"l":"com.google.common.truth.extensions.proto"},{"l":"com.google.common.truth.extensions.re2j"}]
