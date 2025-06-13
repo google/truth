c6ccddbce194c401cebe7be3f0c26d0d11e3446d
cpovirk <cpovirk@google.com>
2025-06-12 18:52:07 -0700

    [Fail instead of throwing](https://github.com/google/truth/issues/207) for null tables and cells.
    Also, improve the message for `containsCell(presentRow, presentColumn, differentValue)`.
    
    Add tests for a few methods that didn't have them.
    
    And add just a few nullness annotations to the test file.
    
    RELNOTES=n/a
    PiperOrigin-RevId: 770880657

--- core/src/main/java/com/google/common/truth/TableSubject.java
+++ core/src/main/java/com/google/common/truth/TableSubject.java
@@ -15,8 +15,6 @@
  */
 package com.google.common.truth;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.truth.Fact.fact;
 import static com.google.common.truth.Fact.simpleFact;
 
@@ -40,33 +38,49 @@ public final class TableSubject extends Subject {
 
   /** Checks that the actual table is empty. */
   public void isEmpty() {
-    if (!checkNotNull(actual).isEmpty()) {
+    if (actual == null) {
+      failWithActual(simpleFact("expected an empty table"));
+    } else if (!actual.isEmpty()) {
       failWithActual(simpleFact("expected to be empty"));
     }
   }
 
   /** Checks that the actual table is not empty. */
   public void isNotEmpty() {
-    if (checkNotNull(actual).isEmpty()) {
+    if (actual == null) {
+      failWithActual(simpleFact("expected a nonempty table"));
+    } else if (actual.isEmpty()) {
       failWithoutActual(simpleFact("expected not to be empty"));
     }
   }
 
   /** Checks that the actual table has the given size. */
   public void hasSize(int expectedSize) {
-    checkArgument(expectedSize >= 0, "expectedSize(%s) must be >= 0", expectedSize);
-    check("size()").that(checkNotNull(actual).size()).isEqualTo(expectedSize);
+    if (actual == null) {
+      failWithActual(fact("expected a table with size", expectedSize));
+    } else if (expectedSize < 0) {
+      failWithoutActual(
+          simpleFact("expected a table with a negative size, but that is impossible"),
+          fact("expected size", expectedSize),
+          fact("actual size", actual.size()),
+          fact("table was", actualCustomStringRepresentationForPackageMembersToCall()));
+    } else {
+      check("size()").that(actual.size()).isEqualTo(expectedSize);
+    }
   }
 
   /** Checks that the actual table contains a mapping for the given row key and column key. */
   public void contains(@Nullable Object rowKey, @Nullable Object columnKey) {
-    if (!checkNotNull(actual).contains(rowKey, columnKey)) {
+    if (actual == null || !actual.contains(rowKey, columnKey)) {
       /*
        * TODO(cpovirk): Consider including information about whether any cell with the given row
        * *or* column was present.
        */
       failWithActual(
-          simpleFact("expected to contain mapping for row-column key pair"),
+          simpleFact(
+              actual == null
+                  ? "expected a table that contains a mapping for row-column key pair"
+                  : "expected to contain mapping for row-column key pair"),
           fact("row key", rowKey),
           fact("column key", columnKey));
     }
@@ -76,7 +90,12 @@ public final class TableSubject extends Subject {
    * Checks that the actual table does not contain a mapping for the given row key and column key.
    */
   public void doesNotContain(@Nullable Object rowKey, @Nullable Object columnKey) {
-    if (checkNotNull(actual).contains(rowKey, columnKey)) {
+    if (actual == null) {
+      failWithActual(
+          simpleFact("expected a table that does not contain a mapping for row-column key pair"),
+          fact("row key", rowKey),
+          fact("column key", columnKey));
+    } else if (actual.contains(rowKey, columnKey)) {
       failWithoutActual(
           simpleFact("expected not to contain mapping for row-column key pair"),
           fact("row key", rowKey),
@@ -95,9 +114,26 @@ public final class TableSubject extends Subject {
   }
 
   /** Checks that the actual table contains the given cell. */
-  public void containsCell(Cell<?, ?, ?> cell) {
-    checkNotNull(cell);
-    checkNoNeedToDisplayBothValues("cellSet()").that(checkNotNull(actual).cellSet()).contains(cell);
+  public void containsCell(@Nullable Cell<?, ?, ?> cell) {
+    if (cell == null) {
+      failWithoutActual(
+          simpleFact("expected to contain a null cell, but that is impossible"),
+          fact("table was", actualCustomStringRepresentationForPackageMembersToCall()));
+    } else if (actual == null) {
+      failWithActual(
+          simpleFact("expected a table that contains the given cell"),
+          fact("row key", cell.getRowKey()),
+          fact("column key", cell.getColumnKey()),
+          fact("value", cell.getValue()));
+    } else if (actual.contains(cell.getRowKey(), cell.getColumnKey())) {
+      // TODO(cpovirk): For a cell that exists but has a null value, consider a clarification like:
+      // "key is present but with a different value" (from MapSubject.containsEntry)
+      check("get(%s, %s)", cell.getRowKey(), cell.getColumnKey())
+          .that(actual.get(cell.getRowKey(), cell.getColumnKey()))
+          .isEqualTo(cell.getValue());
+    } else {
+      checkNoNeedToDisplayBothValues("cellSet()").that(actual.cellSet()).contains(cell);
+    }
   }
 
   /** Checks that the actual table does not contain the given cell. */
@@ -109,26 +145,47 @@ public final class TableSubject extends Subject {
   }
 
   /** Checks that the actual table does not contain the given cell. */
-  public void doesNotContainCell(Cell<?, ?, ?> cell) {
-    checkNotNull(cell);
-    checkNoNeedToDisplayBothValues("cellSet()")
-        .that(checkNotNull(actual).cellSet())
-        .doesNotContain(cell);
+  public void doesNotContainCell(@Nullable Cell<?, ?, ?> cell) {
+    if (cell == null) {
+      failWithoutActual(
+          simpleFact("refusing to check for a null cell because tables never contain null cells"),
+          fact("table was", actualCustomStringRepresentationForPackageMembersToCall()));
+    } else if (actual == null) {
+      failWithActual(
+          simpleFact("expected a table that does not contain the given cell"),
+          fact("row key", cell.getRowKey()),
+          fact("column key", cell.getColumnKey()),
+          fact("value", cell.getValue()));
+    } else {
+      checkNoNeedToDisplayBothValues("cellSet()").that(actual.cellSet()).doesNotContain(cell);
+    }
   }
 
   /** Checks that the actual table contains the given row key. */
   public void containsRow(@Nullable Object rowKey) {
-    check("rowKeySet()").that(checkNotNull(actual).rowKeySet()).contains(rowKey);
+    if (actual == null) {
+      failWithActual(fact("expected a table with row", rowKey));
+    } else {
+      check("rowKeySet()").that(actual.rowKeySet()).contains(rowKey);
+    }
   }
 
   /** Checks that the actual table contains the given column key. */
   public void containsColumn(@Nullable Object columnKey) {
-    check("columnKeySet()").that(checkNotNull(actual).columnKeySet()).contains(columnKey);
+    if (actual == null) {
+      failWithActual(fact("expected a table with column", columnKey));
+    } else {
+      check("columnKeySet()").that(actual.columnKeySet()).contains(columnKey);
+    }
   }
 
   /** Checks that the actual table contains the given value. */
   public void containsValue(@Nullable Object value) {
-    check("values()").that(checkNotNull(actual).values()).contains(value);
+    if (actual == null) {
+      failWithActual(fact("expected a table with value", value));
+    } else {
+      check("values()").that(actual.values()).contains(value);
+    }
   }
 
   static Factory<TableSubject, Table<?, ?, ?>> tables() {
--- core/src/test/java/com/google/common/truth/TableSubjectTest.java
+++ core/src/test/java/com/google/common/truth/TableSubjectTest.java
@@ -15,16 +15,17 @@
  */
 package com.google.common.truth;
 
+import static com.google.common.collect.Tables.immutableCell;
 import static com.google.common.truth.ExpectFailure.assertThat;
 import static com.google.common.truth.ExpectFailure.expectFailure;
 import static com.google.common.truth.FailureAssertions.assertFailureKeys;
 import static com.google.common.truth.FailureAssertions.assertFailureValue;
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
 
 import com.google.common.collect.ImmutableTable;
+import com.google.common.collect.Table;
 import com.google.common.collect.Table.Cell;
-import com.google.common.collect.Tables;
+import org.jspecify.annotations.Nullable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -51,6 +52,13 @@ public class TableSubjectTest {
   }
 
   @Test
+  public void tableIsEmptyOnNullTable() {
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that((Table<?, ?, ?>) null).isEmpty());
+    assertFailureKeys(e, "expected an empty table", "but was");
+  }
+
+  @Test
   public void tableIsNotEmpty() {
     ImmutableTable<Integer, Integer, Integer> table = ImmutableTable.of(1, 5, 7);
     assertThat(table).isNotEmpty();
@@ -61,6 +69,13 @@ public class TableSubjectTest {
     ImmutableTable<Integer, Integer, Integer> table = ImmutableTable.of();
     AssertionError e = expectFailure(whenTesting -> whenTesting.that(table).isNotEmpty());
     assertFailureKeys(e, "expected not to be empty");
+  }
+
+  @Test
+  public void tableIsNotEmptyOnNullTable() {
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that((Table<?, ?, ?>) null).isNotEmpty());
+    assertFailureKeys(e, "expected a nonempty table", "but was");
   }
 
   @Test
@@ -75,8 +90,25 @@ public class TableSubjectTest {
 
   @Test
   public void hasSizeNegative() {
-    assertThrows(
-        IllegalArgumentException.class, () -> assertThat(ImmutableTable.of(1, 2, 3)).hasSize(-1));
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that(ImmutableTable.of(1, 2, 3)).hasSize(-1));
+    assertFailureKeys(
+        e,
+        "expected a table with a negative size, but that is impossible",
+        "expected size",
+        "actual size",
+        "table was");
+    assertFailureValue(e, "expected size", "-1");
+    assertFailureValue(e, "actual size", "1");
+    assertFailureValue(e, "table was", "{1={2=3}}");
+  }
+
+  @Test
+  public void hasSizeOnNullTable() {
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that((Table<?, ?, ?>) null).hasSize(1));
+    assertFailureKeys(e, "expected a table with size", "but was");
+    assertFailureValue(e, "expected a table with size", "1");
   }
 
   @Test
@@ -99,6 +131,20 @@ public class TableSubjectTest {
             "but was");
     assertThat(e).factValue("row key").isEqualTo("row");
     assertThat(e).factValue("column key").isEqualTo("otherCol");
+  }
+
+  @Test
+  public void containsOnNullTable() {
+    AssertionError e =
+        expectFailure(
+            whenTesting -> whenTesting.that((Table<?, ?, ?>) null).contains("row", "otherCol"));
+    assertThat(e)
+        .factKeys()
+        .containsExactly(
+            "expected a table that contains a mapping for row-column key pair",
+            "row key",
+            "column key",
+            "but was");
   }
 
   @Test
@@ -129,6 +175,21 @@ public class TableSubjectTest {
   }
 
   @Test
+  public void doesNotContainOnNullTable() {
+    AssertionError e =
+        expectFailure(
+            whenTesting ->
+                whenTesting.that((Table<?, ?, ?>) null).doesNotContain("row", "otherCol"));
+    assertThat(e)
+        .factKeys()
+        .containsExactly(
+            "expected a table that does not contain a mapping for row-column key pair",
+            "row key",
+            "column key",
+            "but was");
+  }
+
+  @Test
   public void containsCell() {
     ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
     assertThat(table).containsCell("row", "col", "val");
@@ -144,6 +205,44 @@ public class TableSubjectTest {
     assertFailureValue(e, "value of", "table.cellSet()");
     assertFailureValue(e, "expected to contain", "(row,row)=val");
     assertFailureValue(e, "but was", "[(row,col)=val]");
+  }
+
+  @Test
+  public void containsCellFailureWithKeyPairPresent() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that(table).containsCell("row", "col", "var"));
+    assertFailureKeys(e, "value of", "expected", "but was", "table was");
+    assertFailureValue(e, "value of", "table.get(row, col)");
+    assertFailureValue(e, "expected", "var");
+    assertFailureValue(e, "but was", "val");
+  }
+
+  @Test
+  public void containsCellOnNullTable() {
+    AssertionError e =
+        expectFailure(
+            whenTesting ->
+                whenTesting.that((Table<?, ?, ?>) null).containsCell("row", "col", "val"));
+    assertFailureKeys(
+        e,
+        "expected a table that contains the given cell",
+        "row key",
+        "column key",
+        "value",
+        "but was");
+    assertFailureValue(e, "row key", "row");
+    assertFailureValue(e, "column key", "col");
+    assertFailureValue(e, "value", "val");
+    assertFailureValue(e, "but was", "null");
+  }
+
+  @Test
+  public void containsNullCell() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    AssertionError e = expectFailure(whenTesting -> whenTesting.that(table).containsCell(null));
+    assertFailureKeys(e, "expected to contain a null cell, but that is impossible", "table was");
+    assertFailureValue(e, "table was", "{row={col=val}}");
   }
 
   @Test
@@ -171,7 +270,117 @@ public class TableSubjectTest {
     assertFailureValue(e, "but was", "[(row,col)=val]");
   }
 
-  private static <R, C, V> Cell<R, C, V> cell(R row, C col, V val) {
-    return Tables.immutableCell(row, col, val);
+  @Test
+  public void doesNotContainCellOnNullTable() {
+    AssertionError e =
+        expectFailure(
+            whenTesting ->
+                whenTesting.that((Table<?, ?, ?>) null).doesNotContainCell("row", "col", "val"));
+    assertFailureKeys(
+        e,
+        "expected a table that does not contain the given cell",
+        "row key",
+        "column key",
+        "value",
+        "but was");
+    assertFailureValue(e, "row key", "row");
+    assertFailureValue(e, "column key", "col");
+    assertFailureValue(e, "value", "val");
+    assertFailureValue(e, "but was", "null");
+  }
+
+  @Test
+  public void doesNotContainNullCell() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that(table).doesNotContainCell(null));
+    assertFailureKeys(
+        e,
+        "refusing to check for a null cell because tables never contain null cells",
+        "table was");
+    assertFailureValue(e, "table was", "{row={col=val}}");
+  }
+
+  @Test
+  public void containsRow() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    assertThat(table).containsRow("row");
+  }
+
+  @Test
+  public void containsRowFailure() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that(table).containsRow("your boat"));
+    assertFailureKeys(e, "value of", "expected to contain", "but was", "table was");
+    assertFailureValue(e, "value of", "table.rowKeySet()");
+    assertFailureValue(e, "expected to contain", "your boat");
+    assertFailureValue(e, "but was", "[row]");
+  }
+
+  @Test
+  public void containsRowOnNullTable() {
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that((Table<?, ?, ?>) null).containsRow("row"));
+    assertFailureKeys(e, "expected a table with row", "but was");
+    assertFailureValue(e, "expected a table with row", "row");
+    assertFailureValue(e, "but was", "null");
+  }
+
+  @Test
+  public void containsColumn() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    assertThat(table).containsColumn("col");
+  }
+
+  @Test
+  public void containsColumnFailure() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that(table).containsColumn("response"));
+    assertFailureKeys(e, "value of", "expected to contain", "but was", "table was");
+    assertFailureValue(e, "value of", "table.columnKeySet()");
+    assertFailureValue(e, "expected to contain", "response");
+    assertFailureValue(e, "but was", "[col]");
+  }
+
+  @Test
+  public void containsColumnOnNullTable() {
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that((Table<?, ?, ?>) null).containsColumn("col"));
+    assertFailureKeys(e, "expected a table with column", "but was");
+    assertFailureValue(e, "expected a table with column", "col");
+    assertFailureValue(e, "but was", "null");
+  }
+
+  @Test
+  public void containsValue() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    assertThat(table).containsValue("val");
+  }
+
+  @Test
+  public void containsValueFailure() {
+    ImmutableTable<String, String, String> table = ImmutableTable.of("row", "col", "val");
+    AssertionError e = expectFailure(whenTesting -> whenTesting.that(table).containsValue("var"));
+    assertFailureKeys(e, "value of", "expected to contain", "but was", "table was");
+    assertFailureValue(e, "value of", "table.values()");
+    assertFailureValue(e, "expected to contain", "var");
+    assertFailureValue(e, "but was", "[val]");
+  }
+
+  @Test
+  public void containsValueOnNullTable() {
+    AssertionError e =
+        expectFailure(whenTesting -> whenTesting.that((Table<?, ?, ?>) null).containsValue("val"));
+    assertFailureKeys(e, "expected a table with value", "but was");
+    assertFailureValue(e, "expected a table with value", "val");
+    assertFailureValue(e, "but was", "null");
+  }
+
+  private static <
+          R extends @Nullable Object, C extends @Nullable Object, V extends @Nullable Object>
+      Cell<R, C, V> cell(R row, C col, V val) {
+    return immutableCell(row, col, val);
   }
 }
